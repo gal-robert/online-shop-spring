7f6c232785fba5c951f9d9e1c5d98b7a95c306c2
Robert Gal <robyka2009@gmail.com>
2020-02-19 18:57:49 +0200

    Initial commit (project structure)

---
+++ src/main/java/com/galrobert/onlineshop/OnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.galrobert.onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopApplication.class, args);
+	}
+
+}
